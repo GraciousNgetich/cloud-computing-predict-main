71233027c72d077cf56014eff99aacbdced678dc
gracious_ngetich <76058373+GraciousNgetich@users.noreply.github.com>
2022-10-24 11:46:12 +0300

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://d2k0jqd9za.execute-api.eu-west-1.amazonaws.com/portfolio-lambda",
+                url: "https://ykg2bkcq0d.execute-api.eu-west-1.amazonaws.com/portfolio-lambda",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
