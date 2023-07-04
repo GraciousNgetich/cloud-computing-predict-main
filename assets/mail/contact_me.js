5bdf9a675149c7b4d12e5381f470a366f8665c19
Gracious Ngetich <graciousngetich@gmail.com>
2023-07-04 17:04:24 +0300

    ..

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
       var json = { name: name, phone: phone, email: email, message: message };
       $.ajax({
         // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  --------
-        url: "https://5biuzoq465.execute-api.eu-west-1.amazonaws.com/explore-lambda",
+        url: "https://xssgqoa93l.execute-api.eu-west-1.amazonaws.com/default/de_track_lambda",
         // ------------------------------------------------------
         type: "POST",
         data: JSON.stringify(json),
